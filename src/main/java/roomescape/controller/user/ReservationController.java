12a80063dee7e8e32b589478d3fcdae870af1069
sauter001 <lsj457@naver.com>
2026-05-19 17:36:42 +0900

    feat: @LoginUser 적용 및 예약 조회/생성 로직 사용자 정보 기반으로 수정

--- src/main/java/roomescape/controller/user/ReservationController.java
+++ src/main/java/roomescape/controller/user/ReservationController.java
@@ -10,9 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.annotation.LoginUser;
 import roomescape.domain.Reservation;
+import roomescape.domain.User;
 import roomescape.policy.UserReservationCancelPolicy;
 import roomescape.policy.UserReservationSavePolicy;
 import roomescape.request.ReservationEditRequest;
@@ -42,15 +43,18 @@ public class ReservationController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ReservationResponse>> getReservations(@RequestParam("name") String name) {
-        List<ReservationResponse> responses = ReservationResponse.from(reservationService.findReservationsByName(name));
+    public ResponseEntity<List<ReservationResponse>> getReservations(@LoginUser User user) {
+        List<ReservationResponse> responses = ReservationResponse.from(reservationService.findReservationsByName(user.name()));
         return ResponseEntity.ok(responses);
     }
 
     @PostMapping
-    public ResponseEntity<ReservationResponse> saveReservation(@Valid @RequestBody ReservationRequest request) {
+    public ResponseEntity<ReservationResponse> saveReservation(
+            @Valid @RequestBody ReservationRequest request,
+            @LoginUser User user) {
         LocalDateTime now = LocalDateTime.now(clock);
-        Reservation reservationReturned = reservationService.saveReservation(request.toSaveCommand(), now, SAVE_POLICY);
+        Reservation reservationReturned = reservationService.saveReservation(request.toSaveCommand(user.name()), now,
+                SAVE_POLICY);
         ReservationResponse reservationResponse = ReservationResponse.from(reservationReturned);
 
         return ResponseEntity.created(getLocation(reservationResponse.id())).body(reservationResponse);
--- src/main/java/roomescape/request/ReservationRequest.java
+++ src/main/java/roomescape/request/ReservationRequest.java
@@ -1,19 +1,16 @@
 package roomescape.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import roomescape.command.ReservationSaveCommand;
 
 import java.time.LocalDate;
 
 public record ReservationRequest(
-        @NotBlank @Size(max = 255) String name,
         @NotNull LocalDate date,
         @NotNull Long timeId,
         @NotNull Long themeId) {
 
-    public ReservationSaveCommand toSaveCommand() {
-        return new ReservationSaveCommand(name, date, timeId, themeId);
+    public ReservationSaveCommand toSaveCommand(String username) {
+        return new ReservationSaveCommand(username, date, timeId, themeId);
     }
 }

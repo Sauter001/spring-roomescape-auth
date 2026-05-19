fb8b01ebfb3acc566206a84d539fa80c99072779
sauter001 <lsj457@naver.com>
2026-05-19 16:26:48 +0900

    chore: 사용자 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -38,3 +38,12 @@ CREATE TABLE canceled_reservation
     canceled_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE users
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    uid      VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
